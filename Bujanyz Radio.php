405df88728778c5f764030d27594778c1b57c6b5
Kwesiamartey <kwesiamartey@yahoo.com>
2024-07-18 20:34:06 +0000

    Add files via upload

---
+++ Bujanyz Radio.php
@@ -0,0 +1,20 @@
+<Html>
+    
+       <body>
+       
+             <h5 style='text-align: center;'>
+                 <img src='https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif' alt='Girl in a jacket' width='100' height='100'/>
+             </h5>
+        
+        
+             <h5 style='text-align: center;'>
+              <audio controls
+                     src='http://node-25.zeno.fm/2beb7s91rtzuv?rj-ttl=5&rj-tok=AAABeNqXQPoAYrR4KtliaLWy6Q' 
+                     type="audio/mpeg">
+                     Your browser does not support the
+              <code>audio</code> element.
+               </audio>
+             </h5>
+       </body>
+</Html>
+
---
+++ Fox 97.9 FM.php
@@ -0,0 +1,20 @@
+
+<Html>
+    
+       <body>
+       
+             <h5 style='text-align: center;'>
+                   <img src='https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif' alt='Girl in a jacket' width='100' height='100'/>
+             </h5>
+        
+        
+             <h5 style='text-align: center;'>
+              <audio controls
+                     src='http://foxfm.atunwadigital.streamguys1.com/foxfm?amsparams=playerid%3ASGplayer%3Bskey%3A1578575319807%3B&awparams=playerid%3ASGplayer%3B' 
+                     type="audio/mpeg">
+                     Your browser does not support the
+              <code>audio</code> element.
+               </audio>
+             </h5>
+       </body>
+</Html>
