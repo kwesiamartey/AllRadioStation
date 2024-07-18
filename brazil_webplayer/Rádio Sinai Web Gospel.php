06fba35877ad1e80590416f8c4ed52fd33f50c0d
Kwesiamartey <kwesiamartey@yahoo.com>
2024-07-18 21:41:27 +0000

    Add files via upload

---
+++ brazil_webplayer/Rádio Sinai Web Gospel.php
@@ -0,0 +1,36 @@
+
+
+
+<Html>
+    
+       <body>
+       
+             <h5 style='text-align: center;'>
+                 <img src='https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif' alt='Girl in a jacket' width='100' height='100'/>
+             </h5>
+        
+             <h5 style='text-align: center;'>
+             <!--<video id="video" controls type="application/x-mpegURL"></video>-->
+                <audio controls>
+                    <source src="https://centova4.transmissaodigital.com:20278/stream?1690930067215" type="audio/aac">
+                   <!--Fallback source for browsers that don't support AAC-->
+                    <source src="fallback.mp3" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+              
+             </h5>
+       </body>
+</Html>
+<script src='https://cdn.jsdelivr.net/npm/hls.js@latest'></script>
+<script>
+                      if(Hls.isSupported()) {
+                        var video = document.getElementById('video');
+                        var hls = new Hls();
+                        hls.loadSource('https://d233lioynu5inr.cloudfront.net/out/v1/c45e4e4af26a4d59b5d166c2a9ac2bbc/index.m3u8');
+                                      
+                        hls.attachMedia(video);
+                        hls.on(Hls.Events.MANIFEST_PARSED,function() {
+                          video.play();
+                      });
+                     }
+</script>
---
+++ brazil_webplayer/Transcontinental FM 104.7.php
@@ -0,0 +1,24 @@
+<?php
+
+
+   $img_link = "https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif";
+   $audio_link = "https://f38.fabricahost.com.br/transcontinentalsp48?f=1692142484N01H7XRTF3DRS8DZFX7HP4HFVZ7&tid=01H7XQ13RQZA90TZ00QR5VHKSG";
+   $html = "
+       <body>
+       
+             <h1 style='text-align: center;'><img src='$img_link' alt='Girl in a jacket' width='100' height='100'/></h1>
+             
+             <h1 style='text-align: center;'>Transcontinental FM 104.7</h1>
+             <h1 style='text-align: center;'>
+                <audio controls>
+                  <source src='$audio_link' type='audio/ogg'>
+                  <source src='$audio_link' type='audio/mpeg'>
+                  Your browser does not support the audio element.
+                </audio>
+             </h1>
+             
+       </body>
+   ";
+   
+   echo $html
+?>
