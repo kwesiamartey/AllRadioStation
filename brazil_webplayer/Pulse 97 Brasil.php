8532cf52c43acf87f23db61c418423dc4c9d9adb
Kwesiamartey <kwesiamartey@yahoo.com>
2024-07-18 21:44:03 +0000

    Add files via upload

---
+++ brazil_webplayer/Pulse 97 Brasil.php
@@ -0,0 +1,17 @@
+<Html>
+    
+       <body>
+       
+             <h5 style='text-align: center;'>
+                 <img src='https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif' alt='Girl in a jacket' width='100' height='100'/>
+             </h5>
+        
+             <h5 style='text-align: center;'>
+              <audio controls
+                     src='https://stream-153.zeno.fm/3v10z8c9ztzuv?zs=nPDDqhZsR_idm-V-6_sy-A&aw_0_req_lsid=a833156bb26890690cf3e9145dc090f8&triton-uid=cookie%3A0e5aa479-2041-4b75-bdd4-807c2171573b' type="audio/mpeg">
+                     Your browser does not support the
+              <code>audio</code> element.
+               </audio>
+             </h5>
+       </body>
+</Html>
---
+++ brazil_webplayer/otima FM 90.3.php
@@ -0,0 +1,17 @@
+<Html>
+    
+       <body>
+       
+             <h5 style='text-align: center;'>
+                 <img src='https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif' alt='Girl in a jacket' width='100' height='100'/>
+             </h5>
+        
+             <h5 style='text-align: center;'>
+                <audio controls
+                     src='https://f11.fabricahost.com.br/otimafm903?f=1651543989N2a8c496a0118dae41e3aea6c884d56f0&r=aHR0cHM6Ly9zb2NpYWxyYWRpby5jb20uYnIv' type="audio/mpeg">
+                     Your browser does not support the
+              <code>audio</code> element.
+</audio>
+             </h5>
+       </body>
+</Html>
