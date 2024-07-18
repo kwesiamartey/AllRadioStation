1814398dc95ac5960c4098d3624079761784a4c7
Kwesiamartey <kwesiamartey@yahoo.com>
2024-07-18 22:14:06 +0000

    Add files via upload

---
+++ usa_webplayer/rivadavia.php
@@ -0,0 +1,18 @@
+<?php
+
+
+   $img_link = "http://stnfoundation.com/playstore/brazil_logo/logo_2022.png";
+   $audio_link = "Radio Rivadavia 630 AM";
+   $html = "
+       <body>
+       
+             
+        
+                <iframe src='https://rivadavia.com.ar/vivo/' width='100%' height='100%' />
+         
+             
+       </body>
+   ";
+   
+   echo $html
+?>
