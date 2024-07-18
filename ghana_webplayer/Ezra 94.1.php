28c855805d12f04ac9b502ea6cd4811e0e0d59a4
Kwesiamartey <kwesiamartey@yahoo.com>
2024-07-18 21:22:43 +0000

    Add files via upload

---
+++ ghana_webplayer/Ezra 94.1.php
@@ -0,0 +1,19 @@
+<Html>
+    
+       <body>
+       
+             <h5 style='text-align: center;'>
+                 <img src='https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif' alt='Girl in a jacket' width='100' height='100'/>
+             </h5>
+        
+        
+             <h5 style='text-align: center;'>
+              <audio controls
+                     src='http://node-21.zeno.fm/tefxxqzhp2zuv?rj-ttl=5&rj-tok=AAABeNeo77oA9MbrRBP3ovMwKA' 
+                     type="audio/mpeg">
+                     Your browser does not support the
+              <code>audio</code> element.
+               </audio>
+             </h5>
+       </body>
+</Html>
---
+++ ghana_webplayer/Kumasi FM.php
@@ -0,0 +1,20 @@
+
+<Html>
+    
+       <body>
+       
+             <h5 style='text-align: center;'>
+                   <img src='https://structuredappsstreaming.win/9186eb0e475eeff5c6856e8ed43eb31e_w200.gif' alt='Girl in a jacket' width='100' height='100'/>
+             </h5>
+        
+        
+             <h5 style='text-align: center;'>
+              <audio controls
+                     src='https://stream-40.zeno.fm/49cwxy2n1p8uv?zs=HPWDRACYTGKbkfxmAHJ66g' 
+                     type="audio/mpeg">
+                     Your browser does not support the
+              <code>audio</code> element.
+               </audio>
+             </h5>
+       </body>
+</Html>
